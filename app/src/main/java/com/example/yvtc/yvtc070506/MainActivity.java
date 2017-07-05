2ec0f2205bda686a4ba88333c2f29459bdefa6af
Shaojung <shaojung@gmail.com>
2017-07-05 15:52:45 +0800

    AlertDialog use EditText

--- app/src/main/java/com/example/yvtc/yvtc070506/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc070506/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.yvtc.yvtc070506;
 
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -40,6 +43,26 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "按下略過", Toast.LENGTH_SHORT).show();
             }
         });
+        builder.show();
+    }
+
+    public void click2(View v)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("輸入用對話框");
+        builder.setMessage("請輸入訊息");
+        final EditText ed = new EditText(MainActivity.this);
+        builder.setView(ed);
+
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String str = ed.getText().toString();
+                TextView tv = (TextView) findViewById(R.id.textView);
+                tv.setText(str);
+            }
+        });
+
         builder.show();
     }
 }
