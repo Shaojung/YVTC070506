83831984a0c9a2818fe12850a0412e116aa021d1
Shaojung <shaojung@gmail.com>
2017-07-05 15:34:38 +0800

    AlertDialog

--- app/src/main/java/com/example/yvtc/yvtc070506/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc070506/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,17 +20,18 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setMessage("第一個對話框");
         builder.setTitle("對話框標題");
+        builder.setCancelable(false);
         builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                Toast.makeText(MainActivity.this, "按下確定", Toast.LENGTH_SHORT).show();
             }
         });
 
         builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                Toast.makeText(MainActivity.this, "按下取消", Toast.LENGTH_SHORT).show();
             }
         });
         builder.show();
